2d833679fba01bd51e21c0476787a8e0afb65911
Janatas <toninurmanov@gmail.com>
2026-01-28 12:30:46 +0500

    Processing lesson 28.01.26

--- TASKS/26-what-will-be-logged/start.js
+++ TASKS/26-what-will-be-logged/start.js
@@ -5,11 +5,12 @@
  */
 
 function fn() {
-  console.log('Привет из функции fn')
+  console.log('Привет из функции fn');
 
   return function (a) {
-    console.log(a)
-  }
-}
+    console.log(a);
+  };
+} // Привет из функции fn
+// undefined
 
-fn()(true)
+fn()(true); //
--- TASKS/30-callback-function/start.js
+++ TASKS/30-callback-function/start.js
@@ -6,8 +6,8 @@
  * 2. Нужно ли как-то исправить этот код?
  */
 
-setTimeout(function myFn() {
-  console.log('Привет из функции myFn')
-}, 2000)
+setTimeout(function () {
+  console.log('Привет из функции myFn');
+}, 1000);
 
-myFn()
+// myFn();
